161150aec26858d44c1897347f15212ce63d0227
Yunus <128239091+Botirkhoja@users.noreply.github.com>
2023-07-09 13:04:49 +0300

    Add files via upload

---
+++ object bilan for/app.js
@@ -0,0 +1,38 @@
+// const malumot = {
+//     name: "Ibrohimjon",
+//     famil: "Muhammad",
+//     nomer: 998936677559,
+//     pasport: "AD098",
+//     manzil: {
+//         dom: 98,
+//         padez: 78,
+//         street: "uzbekistan"
+//     }
+// }
+
+// console.log(malumot.manzil.street);
+
+
+
+// for (let i= 0; i <= 100; i++ ){
+//     console.log(i);
+// }
+
+// for (let i=0; i < 10; i++){
+//     console.log("Hello IT");
+// }
+
+
+// for (let i = 0; i <= 100; i++){
+//     if (i === 50) {
+//         break
+//     }
+//   console.log(i);
+// }
+
+for (let i = 10; i > 0; i--){
+    if (i === 5) {
+        continue
+    }
+  console.log(i);
+}
